3f43c71e5e763800f27c8c3b53c41fd5d30b1cfe
ajsya <52182942+ajsya@users.noreply.github.com>
2021-12-21 15:51:44 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import scratchconnect, requests, os, json, time
+import scratchconnect, requests, os, json, time, pytz
 from dotenv import load_dotenv
+from datetime import datetime
+from datetime import date
 
 load_dotenv()
 
@@ -8,7 +10,7 @@ password = os.environ['PASSWORD']
 
 login = scratchconnect.ScratchConnect(username, password)
 user = login.connect_user(username="ajsya")
-project = login.connect_project(581011441) # Connect the project
+project = login.connect_project(619760935, access_unshared=True) # Connect the project
 
 def data():
         user.update_data()
@@ -26,12 +28,24 @@ def data():
         rank_country = data['statistics']['ranks']['country']['followers']
         rank_global = data['statistics']['ranks']['followers']
 
-        return views, loves, favs, followers, rank_country, rank_global, messages
+        tz = pytz.timezone('EST') 
+        current_time = datetime.now(tz)
+        today = date.today()
+
+        time = "{0} @ {1}".format(current_time.strftime("%H:%M"), today.strftime("%m/%d/%y"))
+
+        return views, loves, favs, followers, rank_country, rank_global, messages, time
 
 while True:
     variables = project.connect_cloud_variables()
     
-    #print(data())
+    print(data())
+    encoded = variables.encode_list(list(data()))  # Encode a list
+
+    variables.set_cloud_variable(variable_name='Cloud', value=encoded)
+    print('Updated Data Sent!')
+
+    time.sleep(3600) #an hour delay between updates, can be changed based on user's popularity.
     encoded = variables.encode_list(list(data()))  # Encode a list
 
     variables.set_cloud_variable(variable_name='cloud_variable', value=encoded)
